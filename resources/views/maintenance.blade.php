2522919b3c1eed2d47000d706734fdd98e7dd413
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-08-23 02:50:34 +0700

    maintenis page update

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Situs Sedang Dalam Proses Pembangunan</title>
+  <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon_io/favicon.ico') }}">
   <style>
     body {
       margin: 0;
