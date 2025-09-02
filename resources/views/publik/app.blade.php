a648bf13bc4d67157b347a02ba0c07a78a43a557
raka_pramu <rkapramu25@gmail.com>
2025-09-02 14:19:30 +0700

    [FIX] penyesuaian title & favicon - komunitasrehab-U65VLG

--- resources/views/publik/app.blade.php
+++ resources/views/publik/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Komunitas-Rehab</title>
+    <title>Komunitas Rehab - Website Resmi</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon_io/favicon.ico') }}">
     @include('publik.includes.style')
     @stack('css')
 </head>
